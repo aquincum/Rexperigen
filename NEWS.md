4d3db81a9356c1967cfe5c7de41b21030ad0e09f
aquincum <daniel@szeredi.hu>
2016-03-21 21:19:16 -0400

    tests fixed

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,9 @@
+# Rexperigen 0.2
+
+## Major change
+
+Rexperigen now uses the streaming download for servers of version > 2. This makes it much easier for the server and is the preferred download with newer Experigen server versions.
+
 # Rexperigen 0.1
 
 This is the first release
--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -6,13 +6,13 @@ context("download")
 logoutExperigen()
 
 options(Rexperigen.server = "test.org")
-options(Rexperigen.server.version = "2.2.0")
+options(Rexperigen.server.version = "1.2.0")
 
-test_that("makecsv with no auth", {
+test_that("makecsv with no auth, server 1", {
     with_mock(
         "RCurl::url.exists" = function(...) TRUE,
         "RCurl::getURL" = function(url, ...){
-            expect_equal(url, "http://test.org/makecsv?sourceurl=testurl&experimentName=testname&file=default.csv")
+            expect_equal(url, "http://test.org/makecsv.cgi?sourceurl=testurl&experimentName=testname&file=default.csv")
             "column1\tcolumn2\n1\t2\n"
         },
         res <- downloadExperiment("testurl", "testname")
@@ -21,22 +21,38 @@ test_that("makecsv with no auth", {
     expect_equal(res$column2, 2)
 })
 
-test_that("makecsv with auth", {
+options(Rexperigen.server.version = "2.2.0")
+
+test_that("makecsv with no auth, server 2", {
+    with_mock(
+        "RCurl::url.exists" = function(...) TRUE,
+        "RCurl::getURL" = function(url, ...){
+            expect_equal(url, "http://test.org/streamresults?sourceurl=testurl&experimentName=testname&file=default.csv&ndjson=true")
+            '{"column1": 1, "column2": 2}\n'
+        },
+        res <- downloadExperiment("testurl", "testname")
+    )
+    expect_equal(res$column1, 1)
+    expect_equal(res$column2, 2)
+})
+
+test_that("makecsv with auth, server 2", {
     options(Rexperigen.experimenter = "aa")
     options(Rexperigen.password = "bb")
     with_mock(
         "RCurl::url.exists" = function(...) TRUE,
         "RCurl::getURL" = function(url, username, password, ...){
-            expect_equal(url, "http://test.org/digest/makecsv?sourceurl=testurl&experimentName=testname&file=demographics.csv")
+            expect_equal(url, "http://test.org/digest/streamresults?sourceurl=testurl&experimentName=testname&file=demographics.csv&ndjson=true")
             expect_equal(username, "aa")
             expect_equal(password, "bb")
-            "column1\tcolumn2\n1\t2\n"
+            '{"column1": 1, "column2": 2}\n'
         },
         res <- downloadExperiment("testurl", "testname", "demographics.csv", TRUE)
     )
     expect_equal(res$column1, 1)
     expect_equal(res$column2, 2)
 })
+
 
 test_that("getDestinations", {
     with_mock(
